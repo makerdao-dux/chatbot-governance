b2018b541ddac0ed798ba5426d4d7ebfc577b31a
Rafael Ventura <rafinskipg@gmail.com>
2021-11-08 18:51:51 +0100

    Link

--- src/commands/pollsCommand.js
+++ src/commands/pollsCommand.js
@@ -3,6 +3,18 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const pollsModule = require('../modules/polls');
 const { default: BigNumber } = require('bignumber.js');
 
+function getPollsMessage(polls) {
+    return polls.map(item => {
+
+        // const results = item.tally.results.map(r => {
+        //     return `   - ${r.optionName} - ${r.mkrSupport.toFixed(2)}MKR`
+        // }).join('\n')
+
+        const messagePoll = `- <a href="https://vote.makerdao.com/polling/${item.poll.slug}">${item.poll.title.length > 200 ? item.poll.title.substring(0, 200) + '...' : item.poll.title}</a> 🏆 1st option: ${item.tally.winningOptionName} ${item.tally.winningOptionMKR.toFixed(2)}`
+        return messagePoll
+    }).join('\n')
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('polls')
@@ -16,16 +28,16 @@ module.exports = {
         if (polls.length === 0) {
             interaction.reply({ content: 'No active polls right now', ephemeral: true })
         } else {
-            const pollsMessage = polls.map(item => {
+            
+            const firstPolls = polls.slice(0, 3);
+            const lastPolls = polls.slice(3, polls.length)
+            const firstPollsMessage = getPollsMessage(firstPolls)
 
-                // const results = item.tally.results.map(r => {
-                //     return `   - ${r.optionName} - ${r.mkrSupport.toFixed(2)}MKR`
-                // }).join('\n')
+            interaction.editReply({ content: '```css\n'+ firstPollsMessage+ '```', ephemeral: true })
 
-                const messagePoll = `- <a href="https://vote.makerdao.com/polling/${item.poll.slug}">${item.poll.title.length > 200 ? item.poll.title.substring(0, 200) + '...' : item.poll.title}</a> 🏆 1st option: ${item.tally.winningOptionName} ${item.tally.winningOptionMKR.toFixed(2)}`
-                return messagePoll
-            }).join('\n')
-            interaction.editReply({ content: '```css\n'+ pollsMessage+ '```', ephemeral: true })
+            if (lastPolls.length > 0 ) {
+                interaction.reply({ content: '```css\n'+ getPollsMessage(lastPolls)+ '```', ephemeral: true })
+            }
         }
 
 
