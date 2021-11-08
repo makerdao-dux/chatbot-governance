fb2099ead6587efff4313758a80c67eb29b731c1
Rafael Ventura <rafinskipg@gmail.com>
2021-11-08 19:17:12 +0100

    Changelog

--- src/modules/changelog.js
+++ src/modules/changelog.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 function fetchChangelog(key) {
     return axios.get('https://changelog.makerdao.com/releases/mainnet/1.9.9/contracts.json')
         .then(resp => {
-            return resp.data[key] || 'Invalid changelog key, please, try using things like: ' + Object.keys(res.data).slice(0, 4).join(', ') + '... \n Or check at https://changelog.makerdao.com/releases/mainnet/1.9.9/contracts.json '
+            return resp.data[key] || 'Invalid changelog key, please, try using things like: ' + Object.keys(resp.data).slice(0, 4).join(', ') + '... \n Or check at https://changelog.makerdao.com/releases/mainnet/1.9.9/contracts.json '
         })
 }
 
