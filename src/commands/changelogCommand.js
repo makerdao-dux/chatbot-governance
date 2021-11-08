afe34d40698949d9701514a22128720394cb5415
Rafael Ventura <rafinskipg@gmail.com>
2021-11-08 19:14:58 +0100

    Changelog

--- src/commands/changelogCommand.js
+++ src/commands/changelogCommand.js
@@ -6,10 +6,13 @@ const { fetchChangelog } = require('../modules/changelog');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('changelog')
-        .setDescription('Replies with changelog'),
+        .setDescription('Replies with changelog')
+        .addStringOption(option => option.setName('key').setDescription('Enter a key for the changelog')),
     async execute(interaction) {
+        const key = interaction.options.getString('key');
+        
         interaction.reply({ content: 'Getting the changelog...', ephemeral: true })
-        const data = await fetchChangelog()
+        const data = await fetchChangelog(key)
         interaction.editReply({ content: data, ephemeral: true })
 
         
--- src/modules/changelog.js
+++ src/modules/changelog.js
@@ -1,9 +1,9 @@
 const { default: axios } = require("axios");
 
-function fetchChangelog() {
+function fetchChangelog(key) {
     return axios.get('https://changelog.makerdao.com/releases/mainnet/1.9.9/contracts.json')
         .then(resp => {
-            return resp.data.CHANGELOG
+            return resp.data[key] || 'Invalid changelog key, please, try using things like: ' + Object.keys(res.data).slice(0, 4).join(', ') + '... \n Or check at https://changelog.makerdao.com/releases/mainnet/1.9.9/contracts.json '
         })
 }
 
