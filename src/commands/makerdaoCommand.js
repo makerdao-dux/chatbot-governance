b333738210586faf7c203f9646b06acfc9a8945b
grandizzy <grandizzy@localhost>
2021-11-18 14:57:42 +0200

    Add crown to facilitators for /makerdao command too

--- src/commands/makerdaoCommand.js
+++ src/commands/makerdaoCommand.js
@@ -10,8 +10,17 @@ module.exports = {
 	interaction.reply({ content: '```\nMakerDAO Core Units:\n```', ephemeral: true } )
 	var roles = await interaction.guild.roles.fetch();
 	roles.filter((role) => role.name.endsWith('Member') && !role.name.startsWith('Core')).forEach(function(role, index, array) {
-          const cu = `${role.members.map(m=>'- '+m.user.username+' <@'+m.user.id+'>').join('\n')}`
-          interaction.followUp({ embeds: [{"title": role.name.toUpperCase()+'S', "description": cu, "color": 15258703}], ephemeral: true } )
+	  var members = []
+	  role.members.forEach(function(member) {
+	    const isFacilitator = member.roles.cache.some(role => role.name.endsWith('Facilitator'))
+	    var member = '- '+member.user.username+' <@'+member.id+'> '
+	    if (isFacilitator) {
+	       member += ':crown:'
+	    }
+	    members.push(member)
+	  })
+          const message = `${members.join('\n')}`
+          interaction.followUp({ embeds: [{"title": role.name.toUpperCase()+'S', "description": message, "color": 15258703}], ephemeral: true } )
 	})
     }
 }
